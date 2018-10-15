6ba06f171ca3cac507c002d524a99b0e4a71f2e8
Dr. Lionel Saliou <lionel.saliou@ge.com>
2018-10-15 13:42:20 +0100

    Record ngSwith and ngClass example
    ngStyle and ngIf were practiced/covered directly using the
    practice web-site linked to the course.
    
       --- Dr. Lionel Saliou, Ph.D

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,6 +4,8 @@ import { Component, Input } from '@angular/core' ;
   selector: 'event-thumbnail',
   templateUrl : './event-thumbnail.component.html',
   styles: [`
+    .green { color: #003300 !important; }
+    .bold { font-weight: bold; }
     .pad-left { margin-left: 10px ; }
     .well div{ color: #bbb; }
     .thumbnail { min-height: 210px; }
@@ -11,4 +13,16 @@ import { Component, Input } from '@angular/core' ;
 })
 export class EventThumbnailComponent {
   @Input() input_event: any ;
+
+  getStartTimeClass() {
+    const isEarlyStart = this.input_event && this.input_event.time === '8:00 am';
+    return {green: isEarlyStart , bold: isEarlyStart };
+  }
+  getStartTimeStyle(): any {
+    // To be used with the [ngStyle] binding on the appropriate HTML element of the template.
+    if (this.input_event && this.input_event.time === '8:00 am') {
+      return { color: '#003300', 'font-weight': 'bold'}
+    }
+    return {} ;
+  }
 }
