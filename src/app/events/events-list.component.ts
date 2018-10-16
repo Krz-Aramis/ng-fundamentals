eb3363bd1206df9de86a93a18c2342fc0c81269e
Dr. Lionel Saliou <lionel.saliou@ge.com>
2018-10-16 17:06:46 +0100

    Implemented service wrapper for Toastr

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core' ;
 import { EventService } from './shared/event.service' ;
+import { ToastrService } from '../common/toastr.service';
 
 @Component({
   selector: 'events-list',
@@ -9,9 +10,13 @@ export class EventsListComponent implements OnInit {
 
   events: any[] ;
 
-  constructor(private eventService: EventService) { }
+  constructor(private eventService: EventService, private toastrService: ToastrService ) { }
 
   ngOnInit() {
     this.events = this.eventService.getEvents();
+  }
+
+  handleThumbnailClick(name: string) {
+    this.toastrService.success(name, 'Selected Event Name:') ;
   }
 }
