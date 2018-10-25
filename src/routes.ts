4599d6e352d81c7730b771d8bc5eb671a0ac224e
Dr. Lionel Saliou <lionel.saliou@ge.com>
2018-10-25 13:28:01 +0100

    Created barrels for easier import
    When using this technique, it is important to also revisit
    parts of the applications that import or reference a lot of
    the components, such as the routes!
    
    In this commit, we had to change the name of one of the file as it
    was not within the standard. As this file was already badly referenced
    elsewhere, the application was failing to compile. It took a couple
    of minutes to locate what the error was. Still, simplifying the
    imports using barrels at this stage made quick work for ths problem.
    
       --- Dr. Lionel Saliou, Ph.D

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router' ;
-import { EventsListComponent} from './app/events/events-list.component' ;
-import { EventDetailsComponent} from './app/events/event-details/event-details.component';
-import { CreateEventComponent } from './app/events/create-event.component';
+
 import { Error404Component } from './app/error/404.component';
-import { EventRouteActivator } from './app/events/event-details/event-route-activator.service';
-import { EventListResolver } from './app/events/event-list.resolver.service';
+
+import {
+  EventsListComponent,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+} from './app/events/index';
 
 export const appRoutes: Routes = [
   { path: 'events/new', component: CreateEventComponent, canDeactivate: [ 'canDeactivateCreateEvent'] },
