b09f6e2694ac5a11637be6498751c186e42c650c
Dr. Lionel Saliou <lionel.saliou@ge.com>
2018-11-07 13:59:52 +0000

    Implement complex content projection
    Now it is possible to place various piece of information using
    the ng-content tag along with specific selectors.
    
    This way we can keep component re-usable and not tie them
    to business logic.
    
        --- Lionel Saliou, Ph.D

--- src/app/common/collapsible-well.component.ts
+++ src/app/common/collapsible-well.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'collapsible-well',
   templateUrl: 'collapsible-well.component.html'
 })
 export class CollapsibleWellComponent implements OnInit {
-  @Input() title: string ;
   visible: boolean = false ;
 
   constructor() { }
