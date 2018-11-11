1af80f922f79c8079fef304c13433e1a00b8abb3
Dr. Lionel Saliou <lionel.saliou@ge.com>
2018-11-11 11:31:38 +0000

    Use login service over HTTP

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@angular/core';
+import {HttpHeaders, HttpClient} from '@angular/common/http';
 import { IUser } from './user.model';
 
 @Injectable()
 export class AuthService {
   currentUser: IUser;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  loginUser(userName: string, passowrd: string) {
+  loginUser(userName: string, password: string) {
+    // the shape of the object below is dependent on the desing
+    // of the server. In this case, the back-end uses Passport
+    // which defines the object with all lowercase keys.
+    let loginInfo = {username: userName, password: password };
+    let options = {
+      headers: new HttpHeaders(
+        {
+          'Content-Type': 'application/json'
+        }
+      )
+    };
+    this.http.post('/api/login', loginInfo, options);
     this.currentUser = {
       id: 1,
       userName: userName,
